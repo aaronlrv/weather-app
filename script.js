d4a8a7b2d1c6120738f77b516c03f37c72dfbbf6
aaronlrv <aaronvitharanage@gmail.com>
2022-07-10 10:43:23 +0400

    Add function to sort through weather data

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 async function dataFetch() {
   let data = await fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=London&APPID=77b6c4aebc71d028b7bba3ec0ead299f",
+    "https://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=77b6c4aebc71d028b7bba3ec0ead299f&units=metric",
     { mode: "cors" }
   );
   console.log(data);
   let jsonData = await data.json();
   console.log(jsonData);
+
+  return jsonData;
 }
 
-dataFetch();
+async function display() {
+  let data = await dataFetch();
+  console.log(data);
+
+  let locationName = await data.name;
+  let weatherTemp = await data.main.temp;
+  let weatherDesc = await data.weather[0].description;
+
+  console.log(locationName);
+  console.log(weatherTemp);
+  console.log(weatherDesc);
+}
+
+display();
